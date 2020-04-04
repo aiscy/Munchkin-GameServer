db09361a249c1c3d6601fb522b1c28d553b2e7dc
Maxim Valeryevich Pavlov <maxpav137@gmail.com>
2020-04-04 23:43:37 +0500

    Add deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,11 @@
+import com.google.protobuf.gradle.*
+
 plugins {
     java
     application
+    idea
     kotlin("jvm") version kotlinVersion
+    id("com.google.protobuf") version "0.8.12"
 }
 
 group = "org.example.munchkin"
@@ -12,7 +16,16 @@ repositories {
 }
 
 dependencies {
+    implementation(Libraries.kotlinxCoroutines)
 
+    implementation(Libraries.grpcNetty)
+    implementation(Libraries.grpcProtobuf)
+    implementation(Libraries.grpcStub)
+
+    implementation(Libraries.krotoPlusCoroutines)
+    implementation(Libraries.protobufJava)
+
+    implementation(Libraries.javaxAnnotation)
 }
 
 configure<JavaPluginConvention> {
@@ -22,6 +35,7 @@ configure<JavaPluginConvention> {
 tasks {
     compileKotlin {
         kotlinOptions.jvmTarget = "11"
+        kotlinOptions.freeCompilerArgs + "-Xuse-experimental=kotlinx.coroutines.ObsoleteCoroutinesApi"
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "11"
@@ -32,3 +46,27 @@ kotlin.sourceSets["main"].kotlin.srcDirs("src/main/kotlin")
 kotlin.sourceSets["test"].kotlin.srcDirs("src/test/kotlin")
 sourceSets["main"].resources.srcDirs("src/main/resources")
 sourceSets["test"].resources.srcDirs("src/test/resources")
+
+protobuf {
+    protoc {
+        artifact = Libraries.protobufCompiler
+    }
+
+    plugins {
+        id("coroutines") {
+            artifact = Libraries.krotoPlusGenGrpcCoro
+        }
+        id("grpc") {
+            artifact = Libraries.grpcProtocGen
+        }
+    }
+
+    generateProtoTasks {
+        all().forEach { task ->
+            task.plugins {
+                id("coroutines")
+                id("grpc")
+            }
+        }
+    }
+}
--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -2,5 +2,22 @@ const val kotlinVersion = "1.3.71"
 
 object Libraries {
     private object Versions {
+        const val grpcVersion = "1.28.0"
+        const val kotlinxCoroutinesVersion = "1.3.5"
+        const val krotoPlusVersion = "0.6.1"
+        const val protobufVersion = "3.11.4"
+        const val javaxAnnotationVersion = "1.3.2"
     }
+
+    const val grpcProtobuf = "io.grpc:grpc-protobuf:${Versions.grpcVersion}"
+    const val grpcStub = "io.grpc:grpc-stub:${Versions.grpcVersion}"
+    const val grpcNetty = "io.grpc:grpc-netty:${Versions.grpcVersion}"
+    const val grpcProtocGen = "io.grpc:protoc-gen-grpc-java:${Versions.grpcVersion}"
+    const val kotlinxCoroutines = "org.jetbrains.kotlinx:kotlinx-coroutines-core:${Versions.kotlinxCoroutinesVersion}"
+    const val krotoPlusCoroutines = "com.github.marcoferrer.krotoplus:kroto-plus-coroutines:${Versions.krotoPlusVersion}"
+    const val krotoPlusGen = "com.github.marcoferrer.krotoplus:protoc-gen-kroto-plus:${Versions.krotoPlusVersion}"
+    const val krotoPlusGenGrpcCoro = "com.github.marcoferrer.krotoplus:protoc-gen-grpc-coroutines:${Versions.krotoPlusVersion}"
+    const val protobufJava = "com.google.protobuf:protobuf-java:${Versions.protobufVersion}"
+    const val protobufCompiler = "com.google.protobuf:protoc:${Versions.protobufVersion}"
+    const val javaxAnnotation = "javax.annotation:javax.annotation-api:${Versions.javaxAnnotationVersion}"
 }
